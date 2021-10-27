f5d40f037dc78490103282ea162461236fc233b7
psw <parksw125368@naver.com>
2021-10-27 13:22:39 +0900

    211027 깊이우선탐색

---
+++ 이론/탐색/DFS_깊이우선탐색/Main.kt
@@ -0,0 +1,36 @@
+import java.util.*
+
+fun main(){
+
+    val graph = HashMap<String,MutableList<String>>()
+    graph["A"] = mutableListOf("B", "C")
+    graph["B"] = mutableListOf("A", "D")
+    graph["C"] = mutableListOf("A", "G", "H", "I")
+    graph["D"] = mutableListOf("B", "E", "F")
+    graph["E"] = mutableListOf("D")
+    graph["F"] = mutableListOf("D")
+    graph["G"] = mutableListOf("C")
+    graph["H"] = mutableListOf("C")
+    graph["I"] = mutableListOf("C", "J")
+    graph["J"] = mutableListOf("I")
+
+    print(DfsSearch().dfsFunc(graph,"A"))
+
+}
+class DfsSearch{
+    fun dfsFunc(graph : HashMap<String, MutableList<String>>, firstNode : String ) : MutableList<String>{
+        val needVisit = mutableListOf<String>()
+        val visited = mutableListOf<String>()
+
+        needVisit.add(firstNode)
+        while(needVisit.isNotEmpty()){
+            val node = needVisit.removeAt(needVisit.size-1)
+            if (!visited.contains(node)){
+                visited.add(node)
+                needVisit.addAll(graph[node] ?: mutableListOf())
+            }
+        }
+        return visited
+    }
+
+}
