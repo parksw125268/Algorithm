d607e84ab5017402c6f936ce9c28e1c55c858033
psw <parksw125368@naver.com>
2021-11-08 09:52:20 +0900

    211108 heap

--- 자료구조/Heap/Main.kt
+++ 자료구조/Heap/Main.kt
@@ -1,3 +1,4 @@
+import com.sun.org.apache.xpath.internal.operations.Bool
 import java.util.*
 
 fun main(){
@@ -7,14 +8,23 @@ fun main(){
     heap1.dataInsert(20)
     heap1.dataInsert(21)
     heap1.dataInsert(3)
+    heap1.dataInsert(24)
     heap1.dataInsert(12)
     heap1.dataInsert(55)
     heap1.dataInsert(25)
     heap1.dataInsert(45)
     heap1.dataInsert(204)
     heap1.printList()
-    println()
-    print(heap1.pop())
+    println(heap1.pop())
+    heap1.printList()
+    println(heap1.pop())
+    heap1.printList()
+    println(heap1.pop())
+    heap1.printList()
+    println(heap1.pop())
+    heap1.printList()
+    println(heap1.pop())
+    heap1.printList()
 }
 class HeapTest{
     private val heapList= mutableListOf<Int?>()
@@ -45,10 +55,46 @@ class HeapTest{
         }
     }
     fun printList(){
-        print(heapList)
+        println(heapList)
+    }
+    private fun hasChild(pIdx : Int):Boolean{
+        return pIdx*2 <= heapList.size-1
     }
     fun pop() : Int{
-        return heapList[1] ?: -1
+        if (this.heapList.size <= 1){
+            return -1
+        }
+        if (this.heapList.size ==2){
+            return heapList[1]!!
+        }
+        val result = heapList[1]
+        //root 루트에 가장 끝의 노드를 넣고 가장 끝 노드는 삭제
+        heapList[1] = heapList.removeAt(heapList.size-1)
+
+        //루트부터 아래로 비교해가며 다시 포지션 세팅
+        var pIdx = 1
+        var cIdx = 1
+        while (hasChild(pIdx)){
+            if (pIdx*2+1 <= heapList.size-1){ //왼쪽 오른쪽 둘다 있는경우
+                //왼쪽 오른쪽 비교
+                if (heapList[pIdx*2]!! > heapList[pIdx*2+1]!!){
+                    cIdx = pIdx*2
+                }else{
+                    cIdx = pIdx*2+1
+                }
+            }else{//왼쪽만 있는경우
+                cIdx = pIdx*2
+            }
+            if (heapList[pIdx]!! < heapList[cIdx]!!){
+                val a = heapList[pIdx]
+                heapList[pIdx] = heapList[cIdx]
+                heapList[cIdx] = a
+                pIdx = cIdx
+            }else{
+                break
+            }
+        }
+        return result!!
     }
 
 
