ef251afd02108ebf6187b449a760fe65ab5c2b1e
CookDas2 <parksw125368@naver.com>
2021-11-30 01:14:23 +0900

    211130

---
+++ 프로그래머스/DFS,BFS/02.Solution.java
@@ -0,0 +1,45 @@
+class Solution{
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        Tree t = new Tree(target);
+        t.insertData(numbers,0,t.head,0);
+        return t.popCount() ;
+    }
+
+    class Tree {
+        private Node head;
+        private int count;
+        private int target;
+        public Tree(int target){
+            this.head = new Node(0);
+            this.count = 0;
+            this.target = target;
+        }
+        private  class Node{
+            Node right;
+            Node left = null;
+            int value;
+            public Node(int value){
+                this.right = null;
+                this.value = value;
+            }
+        }
+
+        public void insertData(int[] array, int idx, Node currNode, int Sum){ //idx에 0 부터 들어감.
+            if(idx == array.length){
+                if (Sum == target){
+                    count ++;
+                }
+                return;
+            }
+            currNode.left = new Node(-array[idx]);
+            currNode.right = new Node(array[idx]);
+            insertData(array, idx+1, currNode.left, Sum+currNode.left.value);
+            insertData(array, idx+1, currNode.right,Sum+currNode.right.value);
+        }
+
+        public int popCount(){
+            return this.count;
+        }
+    }
+}
