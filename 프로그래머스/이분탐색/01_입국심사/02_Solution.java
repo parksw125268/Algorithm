6538bc8ba7193a10fab6750ab6819914bdb355e8
CookDas2 <parksw125368@naver.com>
2021-12-29 00:08:39 +0900

    211229 입국심사 실패2

--- 프로그래머스/이분탐색/01_입국심사/02_Solution.java
+++ 프로그래머스/이분탐색/01_입국심사/02_Solution.java
@@ -1,32 +1,53 @@
+import java.util.*;
+
 class Solution{
     public long solution(int n, int[] times) {
-        long answer = 0;
-        long[] passed = new long[times.length];
-        for (int cnt=0;cnt<n;cnt++){
-            int idx = 0;
-            long min = 0;
-            for(int i=0;i<times.length;i++){
-                if(i==0){
-                    min = passed[i]+times[i];
-                }else{
-                    if(min > passed[i]+times[i]){
-                        min = passed[i]+times[i];
-                        idx = i;
-                    }
-                }
-            }
-            passed[idx] += times[idx];
+        PriorityQueue<Node> passed = new PriorityQueue<>();
+        ArrayList<Node> answerList = new ArrayList<>();
+
+        //1. Queue 에 다 넣기
+        for(int i=0;i<times.length;i++){
+            passed.add(new Node(times[i]));
         }
 
-        for(int i=0;i<times.length;i++){
-            answer = getMax(answer,passed[i]);
+        //2. 최솟값에다가 한개씩 채우기
+        for(int i=0;i<n;i++){
+            Node node1 = passed.poll();
+            node1.plus();
+            passed.add(node1);
         }
-        return answer;
+        //3. 다꺼내서 ArrayList에 넣기
+        while (passed.size() != 0){
+            Node node1 = passed.poll();
+            node1.minus();
+            answerList.add(node1);
+        }
+        Collections.sort(answerList);
+        Node node2 = answerList.get(answerList.size()-1);
+        return node2.time;
     }
-    private long getMax(long a, long b){
-        if (a>b)
-            return a;
-        else
-            return b;
+
+    class Node implements Comparable<Node>{
+        private int howLong; //걸리는 시간
+        private long time;    //총걸린 시간
+        private Node o;
+
+        public Node(int howLong){
+            this.howLong = howLong;
+            this.time = howLong;
+        }
+
+        public void plus(){
+            this.time += this.howLong;
+        }
+        public void minus(){
+            this.time -= this.howLong;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return (int)(this.time-o.time);
+        }
+
     }
 }
