8a61ce3de74de885344350ae97f69f9ad4db9ec6
CookDas2 <parksw125368@naver.com>
2022-01-26 00:16:16 +0900

    220126

---
+++ 이론/병합정렬/220126_Review.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        ArrayList<Integer> aList = new ArrayList<>();
+        for(int i=0;i<20;i++){
+            aList.add((int)(Math.random()*100));
+        }
+        System.out.println(aList);
+        aList = mSort(aList);
+        System.out.println(aList);
+    }
+
+    private static ArrayList<Integer> mSort(ArrayList<Integer> aList){
+        if(aList.size() <= 1){
+            return aList;
+        }
+
+        ArrayList<Integer> left = new ArrayList<>(aList.subList(0,aList.size()/2));
+        ArrayList<Integer> right = new ArrayList<>(aList.subList(aList.size()/2 , aList.size()));
+        left = mSort(left);
+        right = mSort(right);
+        ArrayList<Integer> newList = new ArrayList<>();
+        while(left.size()!=0 && right.size()!=0){
+            if(left.get(0) < right.get(0)){
+                newList.add(left.remove(0));
+            }else{
+                newList.add(right.remove(0));
+            }
+        }
+        if(left.size()!=0){
+            newList.addAll(left);
+        }else{
+            newList.addAll(right);
+        }
+        return newList;
+    }
+}
