409c8a9fbfeaa8e396f4ec49e1534ecfa63d7df1
CookDas2 <parksw125368@naver.com>
2022-02-12 01:21:47 +0900

    220212

--- 프로그래머스/00 내 느낌대로/Level2/22 후보키/2.Solution.java
+++ 프로그래머스/00 내 느낌대로/Level2/22 후보키/2.Solution.java
@@ -2,31 +2,36 @@ import java.util.*; //System.out.println();
 class Solution {
     private String[][] relation;
     private int y,x;
-    private ArrayList<Integer> notPkList = new ArrayList<>();
+    private ArrayList<Integer> pk_List = new ArrayList<>();
     private int[] selected;
     private int selectColCnt;
     private ArrayList<ArrayList<Integer>> ableList = new ArrayList<>();; //가능한 외부키 조합 리스트
-    
+
     public int solution(String[][] relation) {
         this.relation = relation;
         this.y = relation.length;
         this.x = relation[0].length;
-        
-        getNotPrimaryKeyList(); //pk 아닌것들 리스트 받기
+        getPkList();
         bruteForce(); //notPKList 모든경우의수 해보면서 후보키되는것 뽑기
-        
-        int answer = (x - notPkList.size()) + ableList.size();
+
+        int answer = ableList.size();
         return answer;
     }
-    
+
+    private void getPkList(){
+        for(int i=0;i<relation[0].length;i++){
+            pk_List.add(i);
+        }
+    }
+
     private void bruteForce(){
-        for(int i=2;i<=x;i++){
+        for(int i=1;i<=x;i++){
             selectColCnt = i;
             selected = new int[i];
             work(0,0);
         }
     }
-    
+
     private boolean isAvailable(){
         if(ableList.size() == 0){
             return true;
@@ -35,24 +40,24 @@ class Solution {
         for(int i=0;i<selected.length;i++){
             candList.add(selected[i]);
         }
-        for(ArrayList<Integer> aList : ableList){ 
+        for(ArrayList<Integer> aList : ableList){
             boolean temBool = true;
             for(int i=0;i<aList.size();i++){
                 if(!candList.contains(aList.get(i))){
                     temBool = false;
                 }
-            }            
+            }
             if(temBool){
                 return false;
             }
         }
         return true;
     }
-    
+
     private void work(int idx, int start){
         if(idx == selectColCnt){
             if(isAvailable()){
-                if(isCheckCandidateKey()){ 
+                if(isCheckCandidateKey()){// todo 후보키 되는지 체크해서 가능하면 후보키 리스트에 넣기
                     ArrayList<Integer> newList = new ArrayList<>();
                     for(int i : selected){
                         newList.add(i);
@@ -62,30 +67,13 @@ class Solution {
             }
             return;
         }
-        
-        for(int i=start;i<notPkList.size();i++){
-            selected[idx] = notPkList.get(i);
+
+        for(int i=start;i<pk_List.size();i++){
+            selected[idx] = pk_List.get(i);
             work(idx+1,i+1);
         }
     }
-    
-    private void getNotPrimaryKeyList(){
-        for(int i=0;i<x;i++){
-            if(isNotPrimary(i)){
-                notPkList.add(i);
-            }
-        }
-    }
-    
-    private boolean isNotPrimary(int col){
-        HashSet<String> hs =new HashSet<>();
-        for(int i=0;i<y;i++){
-            hs.add(relation[i][col]);
-        }
-        if(hs.size() == y) return false;
-        return true;
-    }
-    
+
     private boolean isCheckCandidateKey(){
         HashSet<String> hs = new HashSet<>();
         for(int i=0;i<y;i++){
