3e361c00354b4a6b4102552225552249abda37bc
CookDas2 <parksw125368@naver.com>
2022-03-07 22:29:36 +0900

    220307

--- 프로그래머스/00 내 느낌대로/Level2/26 점파와 순간이동/2.Solution.java
+++ 프로그래머스/00 내 느낌대로/Level2/26 점파와 순간이동/2.Solution.java
@@ -1,42 +1,16 @@
 import java.util.*;
 
 public class Solution {
-  int n; int[] dist;
   public int solution(int n) {
-    this.dist = new int[n+1];
-    Arrays.fill(dist,Integer.MAX_VALUE);
-    this.n = n;
-    if(n == 0){
-      return 0;
-    }else if(n == 1 || n == 2){
-      return 1;
-    }
-    bfs();
-    int ans = dist[n];
-    return ans;
-  }
-
-  private void bfs(){
-    ArrayList<Integer> que = new ArrayList<>();
-    que.add(0);
-    dist[0]=0;
-    while(!que.isEmpty()){
-      int x = que.remove(0);
-      for(int i=0;i<2;i++){
-        int newX;
-        if(i==0){
-          newX = x+1;
-        if(newX > n) continue;
-          if(dist[newX] <= dist[x]+1) continue;
-          dist[newX] = dist[x]+1;
-        }else{
-          newX = x*2;
-          if(newX > n) continue;
-          if(dist[newX] <= dist[x]) continue;
-          dist[newX] = dist[x];
-        }
-        que.add(newX);
+    int cnt = 0;
+    while(n!=0){
+      if(n%2 == 0){
+        n = n/2;
+      }else{
+        n -= 1;
+        cnt += 1;
       }
     }
+    return cnt;
   }
 }
